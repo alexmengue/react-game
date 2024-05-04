c594f3644f106c30217162c23dff61247ba7c6db
Alex <alexmengue1990@hotmail.com>
2024-05-04 01:13:28 -0300

    added demon and mini-demon

--- src/components/Board/index.tsx
+++ src/components/Board/index.tsx
@@ -3,11 +3,15 @@ import React from 'react';
 import { GAME_SIZE } from '../../settings/constants';
 
 import Hero from '../Hero';
+import MiniDemon from '../MiniDemon';
+import Demon from '../Demon';
 
 const Board = () => {
   return (
     <div>
       <Hero />
+      <MiniDemon />
+      <Demon />
       <img src="./assets/tileset.gif" alt="Game board"  width={GAME_SIZE} height={GAME_SIZE} />
     </div>
   );
