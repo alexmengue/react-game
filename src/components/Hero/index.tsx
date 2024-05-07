b825540a2261712374883ec63de96c2625e93818
Alex <alexmengue1990@hotmail.com>
2024-05-07 01:10:33 -0300

    added hero moviment

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,15 +1,36 @@
 import React from 'react';
+import useEventListener from '@use-it/event-listener';
 
 import { TILE_SIZE, HEAD_OFFSET } from '../../settings/constants';
 
 import './index.css';
 
+const initialPosition = {
+  x: 1,
+  y: 2
+};
+
 const Hero = () => {
+
+  const [positionState, updatePositionState] = React.useState(initialPosition);
+
+  useEventListener('keydown', (event:any) => {
+    if (event.key === 'ArrowLeft') {
+      updatePositionState({ x: positionState.x - 1, y: positionState.y });
+    } else if (event.key === 'ArrowRight') {
+      updatePositionState({ x: positionState.x + 1, y: positionState.y });
+    } else if (event.key === 'ArrowUp') {
+      updatePositionState({ x: positionState.x, y: positionState.y + 1 });
+    } else {
+      updatePositionState({ x: positionState.x, y: positionState.y - 1 });
+    }
+  });
+
   return (
     <div style={{
       position: 'absolute',
-      bottom: TILE_SIZE * 2,
-      left: TILE_SIZE,
+      bottom: TILE_SIZE * positionState.y,
+      left: TILE_SIZE * positionState.x,
       width: TILE_SIZE,
       height: TILE_SIZE + HEAD_OFFSET,
       backgroundImage: 'url(./assets/HERO.png)',
