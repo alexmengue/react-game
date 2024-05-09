e54eba52a15bc74621affb7de5e8f2fd737acc60
Alex <alexmengue1990@hotmail.com>
2024-05-09 00:19:55 -0300

    att hero

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import useEventListener from '@use-it/event-listener';
 
 import { TILE_SIZE, HEAD_OFFSET } from '../../settings/constants';
+import useHeroMoviment from '../../hooks/useHeroMoviment';
 
 import './index.css';
 
@@ -11,29 +11,13 @@ const initialPosition = {
 };
 
 const Hero = () => {
-
-  const [positionState, updatePositionState] = React.useState(initialPosition);
-  const [direction, updateDirection] = React.useState('RIGHT');
-
-  useEventListener('keydown', (event:any) => {
-    if (event.key === 'ArrowLeft') {
-      updatePositionState({ x: positionState.x - 1, y: positionState.y });
-      updateDirection('LEFT');      
-    } else if (event.key === 'ArrowRight') {
-      updatePositionState({ x: positionState.x + 1, y: positionState.y });
-      updateDirection('RIGHT');
-    } else if (event.key === 'ArrowUp') {
-      updatePositionState({ x: positionState.x, y: positionState.y + 1 });
-    } else if (event.key === 'ArrowDown') {
-      updatePositionState({ x: positionState.x, y: positionState.y - 1 });
-    }
-  });
+  const { position, direction } = useHeroMoviment(initialPosition);
 
   return (
     <div style={{
       position: 'absolute',
-      bottom: TILE_SIZE * positionState.y,
-      left: TILE_SIZE * positionState.x,
+      bottom: TILE_SIZE * position.y,
+      left: TILE_SIZE * position.x,
       width: TILE_SIZE,
       height: TILE_SIZE + HEAD_OFFSET,
       backgroundImage: 'url(./assets/HERO.png)',
