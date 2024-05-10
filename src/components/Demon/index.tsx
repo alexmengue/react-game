ad98c55bb46af21ef44d07b0cbdbdb69bf8f8298
Alex <alexmengue1990@hotmail.com>
2024-05-10 19:16:37 -0300

    att demon

--- src/components/Demon/index.tsx
+++ src/components/Demon/index.tsx
@@ -1,20 +1,29 @@
 import React from 'react';
 
 import { TILE_SIZE, DEMON_TILE_SIZE } from '../../settings/constants';
+import useEnemyMoviment from '../../hooks/useEnemyMoviment';
 
 import './index.css';
 
+const initialPosition = {
+  x: 5,
+  y: 5
+};
+
 const Demon = () => {
+  const moviment = useEnemyMoviment(initialPosition);
+
   return (
     <div style={{
       position: 'absolute',
-      bottom: TILE_SIZE * 11,
-      left: TILE_SIZE * 5,
+      bottom: TILE_SIZE * moviment.position.y,
+      left: TILE_SIZE * moviment.position.x,
       width: DEMON_TILE_SIZE,
       height: DEMON_TILE_SIZE,
       backgroundImage: 'url(./assets/DEMON.png)',
       backgroundRepeat: 'no-repeat',
-      animation: 'demon-animation 1s steps(4) infinite'
+      animation: 'demon-animation 1s steps(4) infinite',
+      transform: `scaleX(${moviment.direction === 'RIGHT' ? 1 : -1})`
     }}
     />
   );
---
+++ src/hooks/useEnemyMoviment/index.ts
@@ -0,0 +1,31 @@
+import React from 'react';
+import useEventListener from '@use-it/event-listener';
+
+function useEnemyMoviment (initialPosition) {
+  const [positionState, updatePositionState] = React.useState(initialPosition);
+  const [direction, updateDirection] = React.useState('RIGHT');
+
+  useEventListener('keydown', (event: any) => {
+    if (event.key === 'ArrowLeft') {
+      updatePositionState({ x: positionState.x - 1, y: positionState.y });
+      updateDirection('LEFT');      
+
+    } else if (event.key === 'ArrowRight') {
+      updatePositionState({ x: positionState.x + 1, y: positionState.y });
+      updateDirection('RIGHT');
+
+    } else if (event.key === 'ArrowUp') {
+      updatePositionState({ x: positionState.x, y: positionState.y + 1 });
+      
+    } else if (event.key === 'ArrowDown') {
+      updatePositionState({ x: positionState.x, y: positionState.y - 1 });
+    }
+  });
+
+  return {
+    position: positionState,
+    direction: direction
+  }
+}
+
+export default useEnemyMoviment;
