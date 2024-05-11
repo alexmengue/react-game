1ebc752473724432ac080cd38102c0d455c15523
Alex <alexmengue1990@hotmail.com>
2024-05-11 00:07:39 -0300

    att mini demon

--- src/components/MiniDemon/index.tsx
+++ src/components/MiniDemon/index.tsx
@@ -1,21 +1,30 @@
 import React from 'react';
 
 import { TILE_SIZE, HEAD_OFFSET } from '../../settings/constants';
+import useEnemyMoviment from '../../hooks/useEnemyMoviment';
 
 import './index.css';
 
+const initialPosition = {
+  x: 10,
+  y: 8
+};
+
 const MiniDemon = () => {
+
+  const moviment = useEnemyMoviment(initialPosition);
   return (
     <div style={{
       position: 'absolute',
-      bottom: TILE_SIZE * 5,
-      left: TILE_SIZE * 7,
+      bottom: TILE_SIZE * moviment.position.y,
+      left: TILE_SIZE * moviment.position.x,
       width: TILE_SIZE,
       height: TILE_SIZE + HEAD_OFFSET,
       backgroundImage: 'url(./assets/MINI-DEMON.png)',
       backgroundRepeat: 'no-repeat',
       backgroundPosition: `0 -${TILE_SIZE - HEAD_OFFSET}px`,
-      animation: 'mini-demon-animation 1s steps(4) infinite'
+      animation: 'mini-demon-animation 1s steps(4) infinite',
+      transform: `scaleX(${moviment.direction === 'RIGHT' ? 1 : -1})`
     }}
     />
   );
