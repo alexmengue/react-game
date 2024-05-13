e0d08a02f26e5aa974f524e51a49f593cb014964
Alex <alexmengue1990@hotmail.com>
2024-05-13 01:58:02 -0300

    started with enemies auto-moviment

--- src/hooks/useEnemyMoviment/index.ts
+++ src/hooks/useEnemyMoviment/index.ts
@@ -1,4 +1,5 @@
 import React from 'react';
+import useInterval from '@use-it/interval';
 import useEventListener from '@use-it/event-listener';
 
 import { EDirection } from '../../settings/constants';
@@ -7,22 +8,24 @@ function useEnemyMoviment (initialPosition) {
   const [positionState, updatePositionState] = React.useState(initialPosition);
   const [direction, updateDirection] = React.useState(EDirection.RIGHT);
 
-  useEventListener('keydown', (event: any) => {
-    if (event.key === EDirection.LEFT) {
-      updatePositionState({ x: positionState.x - 1, y: positionState.y });
-      updateDirection(EDirection.LEFT);      
+useInterval(function move() {
+    const random = Math.random() * 3;
 
-    } else if (event.key === EDirection.RIGHT) {
-      updatePositionState({ x: positionState.x + 1, y: positionState.y });
-      updateDirection(EDirection.RIGHT);
-
-    } else if (event.key === EDirection.UP) {
-      updatePositionState({ x: positionState.x, y: positionState.y + 1 });
+    // if (event.key === EDirection.LEFT) {
+    //   updatePositionState({ x: positionState.x - 1, y: positionState.y });
+    //   updateDirection(EDirection.LEFT);      
+  
+    // } else if (event.key === EDirection.RIGHT) {
+    //   updatePositionState({ x: positionState.x + 1, y: positionState.y });
+    //   updateDirection(EDirection.RIGHT);
+  
+    // } else if (event.key === EDirection.UP) {
+    //   updatePositionState({ x: positionState.x, y: positionState.y + 1 });
       
-    } else if (event.key === EDirection.DOWN) {
-      updatePositionState({ x: positionState.x, y: positionState.y - 1 });
-    }
-  });
+    // } else if (event.key === EDirection.DOWN) {
+    //   updatePositionState({ x: positionState.x, y: positionState.y - 1 });
+    // }
+  }, 2000);
 
   return {
     position: positionState,
