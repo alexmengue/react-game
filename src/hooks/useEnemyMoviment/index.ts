c74f226f24096ea039cbf0bf3a517088e41213c7
Alex <alexmengue1990@hotmail.com>
2024-05-15 20:31:56 -0300

    update enemy moviment

--- src/hooks/useEnemyMoviment/index.ts
+++ src/hooks/useEnemyMoviment/index.ts
@@ -1,8 +1,8 @@
 import React from 'react';
 import useInterval from '@use-it/interval';
-import useEventListener from '@use-it/event-listener';
 
 import { EDirection } from '../../settings/constants';
+import { handleNextPosition } from '../../contexts/canvas/helpers';
 
 function useEnemyMoviment (initialPosition) {
   const [positionState, updatePositionState] = React.useState(initialPosition);
@@ -12,21 +12,9 @@ useInterval(function move() {
     const random = Math.floor(Math.random() * 4);
     const directionArray = Object.values(EDirection);
     const randomDirection = directionArray[random];
-
-    if (randomDirection === EDirection.LEFT) {
-      updatePositionState({ x: positionState.x - 1, y: positionState.y });
-      updateDirection(EDirection.LEFT);      
-  
-    } else if (randomDirection === EDirection.RIGHT) {
-      updatePositionState({ x: positionState.x + 1, y: positionState.y });
-      updateDirection(EDirection.RIGHT);
-  
-    } else if (randomDirection === EDirection.UP) {
-      updatePositionState({ x: positionState.x, y: positionState.y + 1 });
-      
-    } else if (randomDirection === EDirection.DOWN) {
-      updatePositionState({ x: positionState.x, y: positionState.y - 1 });
-    }
+    const nextPosition = handleNextPosition(randomDirection, positionState);
+    
+    updatePositionState(nextPosition);
   }, 2000);
 
   return {
