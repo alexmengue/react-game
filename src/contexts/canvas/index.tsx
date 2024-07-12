a599650db63662314ce3543fb928db1f5b72230d
Alex <alexmengue1990@hotmail.com>
2024-07-12 20:58:10 -0300

    update canvas provider

--- src/contexts/canvas/index.tsx
+++ src/contexts/canvas/index.tsx
@@ -1,16 +1,26 @@
 import React from "react";
 import { canvas } from "./helpers";
 
+interface IProps {
+  children: React.ReactNode;
+}
+
 export const CanvasContext = React.createContext({
   canvas: [],
   updateCanvas: () => null
 });
 
-function CanvasProvider() {
+function CanvasProvider(props: IProps) {
   const [canvasState, updateCanvasState] = React.useState({
     canvas: canvas,
     updateCanvas: () => {
       console.log('updateCanvas');
     }
   });
+
+  return (
+    <CanvasContext.Provider value={canvasState}>
+      {props.children}
+    </CanvasContext.Provider>
+  )
 };
