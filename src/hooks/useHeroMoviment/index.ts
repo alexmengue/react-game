ef90cb26f25853784e7d8d01e89e3db65f022da4
Alex <alexmengue1990@hotmail.com>
2024-07-14 23:48:38 -0300

    update canvas context feature

--- src/hooks/useHeroMoviment/index.ts
+++ src/hooks/useHeroMoviment/index.ts
@@ -3,10 +3,14 @@ import useEventListener from '@use-it/event-listener';
 
 import { EDirection, EWalker } from '../../settings/constants';
 import { checkValidMoviment, handleNextPosition } from '../../contexts/canvas/helpers';
+import { CanvasContext } from '../../contexts/canvas';
 
 function useHeroMoviment (initialPosition) {
+  const canvasContext = React.useContext(CanvasContext);
   const [positionState, updatePositionState] = React.useState(initialPosition);
   const [direction, updateDirectionState] = React.useState(EDirection.RIGHT);
+
+  canvasContext.updateCanvas();
 
   useEventListener('keydown', (event: any) => {
     const direction = event.key as EDirection;
