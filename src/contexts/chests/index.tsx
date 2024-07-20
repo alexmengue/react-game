1d2b02fa5ff0e392cae3dc29c1cd2a9bfeb9c411
Alex <alexmengue1990@hotmail.com>
2024-07-19 23:50:51 -0300

    started with chests logic

---
+++ src/contexts/chests/index.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+interface IProps {
+  children: React.ReactNode;
+}
+
+export const ChestsContext = React.createContext({
+  totalChests: 0,
+  openedChests: {
+    total: 0,
+    positions: []
+  },
+  updateOpenedChests: () => null
+});
+
+function ChestsProvider(props: IProps) {
+  const [chestsState, updateChestsState] = React.useState({
+    totalChests: 2,
+    openedChests: {
+      total: 0,
+      positions: []
+    },
+    updateOpenedChests: () => {
+      //soon
+    }
+  });
+
+  return (
+    <ChestsContext value={chestsState}>{props.children}</ChestsContext>
+  )
+}
+
+export default ChestsProvider;
